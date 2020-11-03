c1c457faf2f01bd90ae39d93b96ee0d8761f2c5a
Duane Deane <lilboom@Duanes-Air.attlocal.net>
2020-11-02 20:15:06 -0600

    delete button and cart

--- src/components/productincart/productInCart.jsx
+++ src/components/productincart/productInCart.jsx
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import "./productInCart.css";
+import {removeProductFromCart} from '../../store/actions/actions';
+import { connect } from 'react-redux';
 
 class ProductInCart extends Component {
     state = {  }
     render() { 
-        return <h1>{this.props.data.product.title}</h1>;
+        return (
+            <div className='cart'> 
+                 <h1>{this.props.data.product.title}</h1>
+                 <img src={"/products/"+this.props.data.product.image}></img>
+                 <li>Quantity: {this.props.data.quantity}</li>
+                 <li>Price: ${this.props.data.product.price}</li>
+                 <li>Total: ${this.props.data.quantity * this.props.data.product.price}</li>
+                 <button onClick={this.removeClicked} className="btn btn-sm btn-info">Remove</button>
+            </div>
+        )
+    }
+    removeClicked = () => {
+        console.log("Dispatching action");
+        
+        var removedProduct = {
+            product: this.props.data.product,
+            quantity: this.props.data.quantity
+        };
+
+        this.props.removeProductFromCart(removedProduct);
     }
 }
- 
-export default ProductInCart;
+export default connect(null, {removeProductFromCart})(ProductInCart);
--- src/store/reducers/cartProducts.js
+++ src/store/reducers/cartProducts.js
@@ -1,15 +1,14 @@
 // reducer: a simple fin receives state and action and return a new state
 const cartProductsReducer = (state = [], action) => {
-    if(action.type === "ADD_PRODUCT") {
-        var copy = [...state];
-        copy.push(action.payload);
-        return copy;
-    }
-    else if (action.type === "REMOVE_PRODUCT") {
-        return state.filter(pc => pc.product.id !== action.payload.product.id);
-    }
-    
-    return state;
+  if (action.type === "ADD_PRODUCT") {
+    var copy = [...state];
+    copy.push(action.payload);
+    return copy;
+  } else if (action.type === "REMOVE_PRODUCT") {
+    return state.filter((pc) => pc.product.id !== action.payload.product.id);
+  }
+
+  return state;
 };
 
 export default cartProductsReducer;
