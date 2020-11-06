669cd29e5fae2306b035d4fd8195a7db83e15a50
Duane Deane <lilboom@Duanes-Air.attlocal.net>
2020-11-05 19:13:10 -0600

    Test

--- src/store/reducers/cartCounter.js
+++ src/store/reducers/cartCounter.js
@@ -1,9 +1,11 @@
 // reducer: a simple fn receives state and action and return a new state
 const cartCounterReducer = (state = 0, action) => {
-    if(action.type === "INCREMENT_COUNTER") {
-        return state + 1;
-    }
-    return state;
+  if (action.type === "INCREMENT_COUNTER") {
+    return state + 1;
+  }
+  return state;
 };
+
+// valid change
 
 export default cartCounterReducer;
